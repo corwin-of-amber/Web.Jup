8b315495a0d99b9af46d4d9321b6608201e6c0df
Shachar Itzhaky <corwin.amber@gmail.com>
2025-03-15 12:00:35 +0200

    [refactor] Moved autocompletion code.

---
+++ src/autocomplete.ts
@@ -0,0 +1,27 @@
+import { JupyterConnection } from "./backend/connection";
+
+
+class JupyterAutocomplete {
+
+    jup: JupyterConnection
+
+    constructor(jup: JupyterConnection) {
+        this.jup = jup;
+    }
+
+    async get(prefix: string, word: string) {
+        try {
+            var e: string[] =
+                await this.jup.evalJson(`dir(${prefix})`);
+        }
+        catch {
+            return undefined;
+        }
+        if (!word.startsWith('_'))
+            e = e.filter(w => !w.startsWith('_'));
+        return e.map(w => ({label: w}));
+    }
+}
+
+
+export { JupyterAutocomplete }
